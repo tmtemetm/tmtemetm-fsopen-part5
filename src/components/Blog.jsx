b8c9dc53aea5d60133e49d90c6c01dc51f102ca9
tmtemetm <teemu.j.maki@aalto.fi>
2024-05-23 16:58:03 +0300

    Tehtävä 5.7

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,35 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false)
+
+  const toggleShowDetails = () => {
+    setShowDetails(!showDetails)
+  }
+
+  if (!showDetails) {
+    return (
+      <div className="blog-list-item">
+        {blog.title} {blog.author}&nbsp;
+        <button onClick={toggleShowDetails}>View</button>
+      </div>
+    )
+  }
+
+  return (
+    <div className="blog-list-item">
+      <div>
+        {blog.title}&nbsp;
+        <button onClick={toggleShowDetails}>Hide</button>
+      </div>
+      <div>{blog.url}</div>
+      <div>
+        Likes: {blog.likes}&nbsp;
+        <button>Like</button>
+      </div>
+      <div>{blog.author}</div>
+    </div>
+  )
+}
 
 export default Blog
